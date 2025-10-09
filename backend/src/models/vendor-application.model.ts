d21d55b6f6023c1f0f9815a5c421a3f4dfc9f3ef
Omar-Gamaleldin <omargamal01000001@gmail.com>
2025-10-09 17:25:52 +0300

    feat: finished vendor-application model
    Signed-off-by: Omar-Gamaleldin <omargamal01000001@gmail.com>

---
+++ backend/src/models/vendor-application.model.ts
@@ -0,0 +1,60 @@
+/**
+ * Event Model
+ *
+ * Mongoose schema for Event entity
+ *
+ * @module models/event.model
+ */
+
+import mongoose, { Schema } from "mongoose";
+import { BoothSize, ApplicationType } from "@event-manager/shared";
+import { type IBaseDocument, createBaseSchema } from "./base.model";
+
+export interface IVendorApplication extends IBaseDocument {
+  vendor: mongoose.Types.ObjectId;
+  names: string[];
+
+  emails: string[];
+  boothSize: keyof typeof BoothSize;
+  type: keyof typeof ApplicationType;
+}
+
+const applicationSchema = createBaseSchema<IVendorApplication>({
+  vendor: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  names: {
+    type: [String],
+    validate: {
+      validator: function (v) {
+        return v && v.length >= 1 && v.length <= 5;
+      },
+      message: "Must be between 1 and 5 people",
+    },
+    required: true,
+  },
+  emails: {
+    type: [String],
+    validate: {
+      validator: function (v) {
+        return v && v.length >= 1 && v.length <= 5;
+      },
+      message: "Must be between 1 and 5 emails",
+    },
+    required: true,
+  },
+  boothSize: {
+    type: String,
+    enum: ["2x2", "4x4"],
+  },
+  type: {
+    type: String,
+    enum: ["BAZAAR", "PLATFORM"],
+  },
+});
+
+export const VendorApplication = mongoose.model<IVendorApplication>(
+  "Vendor Application",
+  applicationSchema,
+);
